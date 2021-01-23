89754ee1e6cff7b499fdddef4e30c48e2d43013e
Hossein Mahabadi <hossein.mahabadi@hotmail.com>
2021-01-23 14:17:07 +0330

    1.6.182

---
+++ ShamsiDatePicker/Platform/ios/Renderers/ShamsiDatePickerRenderer.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using UIKit;
+using ShamsiDatePicker.Platform.ios.Renderers;
+
+[assembly: ExportRenderer(typeof(ShamsiDatePicker.ShamsiDatePicker), typeof(ShamsiDatePickerRenderer))]
+namespace ShamsiDatePicker.Platform.ios.Renderers
+{
+    class ShamsiDatePickerRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            // Disabling the keyboard
+            this.Control.InputView = new UIView();
+        }
+    }
+}
